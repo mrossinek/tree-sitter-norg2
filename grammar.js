db33c1c8ffd7ee1ffc3c66abd14e010dcc865b25
Max Rossmannek <max.rossmannek@uzh.ch>
2022-05-04 22:09:52 +0200

    refactor: quote node generator

--- grammar.js
+++ grammar.js
@@ -4,6 +4,10 @@ module.exports = grammar({
     conflicts: $ => [
         [$.quote1],
         [$.quote2],
+        [$.quote3],
+        [$.quote4],
+        [$.quote5],
+        [$.quote6],
     ],
 
     rules: {
@@ -29,44 +33,47 @@ module.exports = grammar({
                 choice(
                     $.quote1,
                     $.quote2,
+                    $.quote3,
+                    $.quote4,
+                    $.quote5,
+                    $.quote6,
                 ),
             ),
         ),
 
-        quote1: $ => prec.dynamic(1,
-            seq(
-                optional($.carryover_tag),
-
-                token.immediate('> '),
-
-                repeat1(
-                    choice(
-                        alias($.word, "_word"),
-                        alias($.line_break, "_line_break"),
-                    ),
-                ),
-
-                repeat(
-                    choice(
-                        $.quote2,
-                    ),
-                ),
-            ),
-        ),
-
-        quote2: $ => prec.dynamic(1,
-            seq(
-                optional($.carryover_tag),
-
-                token.immediate('>> '),
-                repeat1(
-                    choice(
-                        alias($.word, "_word"),
-                        alias($.line_break, "_line_break"),
-                    ),
-                ),
-            ),
-        ),
+        quote1: $ => gen_quote($, 1),
+        quote2: $ => gen_quote($, 2),
+        quote3: $ => gen_quote($, 3),
+        quote4: $ => gen_quote($, 4),
+        quote5: $ => gen_quote($, 5),
+        quote6: $ => gen_quote($, 6),
 
     }
 });
+
+function gen_quote($, level) {
+    lower_level_quotes = [];
+
+    for (let i = 0; i + level < 6; i++) {
+        lower_level_quotes[i] = $["quote" + (i + 1 + level)]
+    }
+
+    return seq(
+        optional($.carryover_tag),
+
+        token.immediate('>'.repeat(level) + ' '),
+
+        repeat1(
+            choice(
+                alias($.word, "_word"),
+                alias($.line_break, "_line_break"),
+            ),
+        ),
+
+        repeat(
+            choice(
+                ...lower_level_quotes,
+            ),
+        ),
+    );
+}
